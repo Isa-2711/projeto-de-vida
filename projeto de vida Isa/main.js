8362c44a1161450c70ffe73566b5faf889621b3a
Isa-2711 <pichler.isadora@escola.pr.gov.br>
2026-06-02 08:39:57 -0300

    Add files via upload

---
+++ projeto de vida Isa/main.js
@@ -0,0 +1,43 @@
+botoes = document.querySelectorAll(".botao");
+const abas = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    // Remove a classe ativo de todos os botões
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+    }
+
+    // Remove a classe ativo de todas as abas
+    for (let j = 0; j < abas.length; j++) {
+      abas[j].classList.remove("ativo");
+    }
+
+    // Adiciona a classe ativo ao botão clicado
+    botoes[i].classList.add("ativo");
+
+    // Adiciona a classe ativo à aba correspondente
+    abas[i].classList.add("ativo");
+
+  };
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2026-10-05T00:00:00");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+    
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+ 
+ return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+  }
